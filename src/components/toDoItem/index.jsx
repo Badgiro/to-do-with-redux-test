58ffb6ba3fe1759491f871ef8eda93e016b2e64a
badgiro <mulbashev98@gmail.com>
2024-10-15 15:09:18 +0300

    first one

--- src/components/toDoItem/index.jsx
+++ src/components/toDoItem/index.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
-import styles from "./style.module.css";
+import { useDispatch } from "react-redux";
 import DeleteItem from "./deleteItem";
+import { editTodo } from "../../store/slices/toDoSlice";
+import styles from "./style.module.css";
+
+
 
 
 
@@ -8,19 +12,45 @@ import DeleteItem from "./deleteItem";
 
 
 const ToDoitem = ({ title, completed, id }) => {
-
+  const dispatch = useDispatch()
   const [isChecked, setIsChecked] = useState(completed);
+  const [isEditing, setIsEditing] = useState(false)
+  const [newTitle, setNewTitle] = useState(title)
+
+  const handleNewTitle = (e) => {
+    setNewTitle(e.target.value)
+  }
+
+
+  const edit = () => {
+    setIsEditing(!isEditing)
+  }
+
 
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
- 
   };
+
+  const saveTitle = ()=> {
+    dispatch(editTodo({title:newTitle, id} ))
+    setIsEditing(false)
+  }
  
 
 
   return (
     <li className={`${styles.item} ${isChecked ? styles.itemChecked : ""}`}>
-      <h1>{title}</h1>
+      {isEditing
+      ? 
+        <input type="text" onChange={handleNewTitle} value={newTitle} onBlur={saveTitle} autoFocus/>
+      : 
+        <h1 onClick={edit}>{title}</h1>
+    
+    }
+
+
+
+      
       <input
         className={styles.item__checkbox}
         type="checkbox"
--- src/store/slices/toDoSlice.js
+++ src/store/slices/toDoSlice.js
@@ -1,26 +1,31 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const toDoSlice = createSlice({
-    name: 'toDo',
-    initialState: {
-        data:[],
-
+  name: 'toDo',
+  initialState: {
+    data: [],
+  },
+  reducers: {
+    addToDo(state, action) {
+      state.data.push({
+        id: Math.random(),
+        title: action.payload.title,
+        completed: false,
+      })
     },
-    reducers: {
-        addToDo(state, action){
-            state.data.push({
-            id: Math.random(),
-            title:action.payload.title, 
-            completed: false
-            })
-           
-        },
-        removeTodo(state, action){
-           state.data =  state.data.filter(toDoItem => 
-            toDoItem.id !== action.payload.id)
-        }
-    }
-
+    removeTodo(state, action) {
+      state.data = state.data.filter(
+        (toDoItem) => toDoItem.id !== action.payload.id
+      )
+    },
+    editTodo(state, action) {
+      const { id, title } = action.payload
+      const todo = state.data.find((todoItem) => todoItem.id === id)
+      if (todo) {
+        todo.title = title
+      }
+    },
+  },
 })
-export const {addToDo, removeTodo}= toDoSlice.actions
+export const { addToDo, removeTodo, editTodo } = toDoSlice.actions
 export default toDoSlice.reducer
